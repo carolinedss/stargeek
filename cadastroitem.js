caa657e37e95ff5ae729a00e1f2d5c53e6f3e3a4
Lívia <livia.caroline@portalsesisp.org.br>
2023-05-26 16:05:09 -0300

    logica 2605

--- cadastroitem.js
+++ cadastroitem.js
@@ -1,8 +1,18 @@
 const nome = document.getElementById("nome");
 const resumo = document.getElementById("resumo");
 const botaocadastrar = document.getElementById("btncadastrar");
+const foto = document.getElementById("foto");
+
+var url = new URL (window.location.href);
+var peditar = url.searchParams.get("peditar");
+var pindice = url.searchParams.get("indice");
+
+if (peditar == "true"){
+    editar(pindice)
+}
 
 botaocadastrar.onclick = (evento) => {
+    if ((peditar != "true")||(peditar == null)){
     evento.preventDefault();
     fenvio()
     .then(result =>{
@@ -21,10 +31,41 @@ botaocadastrar.onclick = (evento) => {
             alert("Houve erro no envio do arquivo");
         }
     });
+
+}
+else
+{
+    editarenvio(evento);
+    window.location.assign("catalogo.html")
+}
 }
 
+function editar(indice){
+    nome.value = "";
+    resumo.value = "";
+    foto.files[0] = null;
+    let dados = JSON.parse(localStorage.getItem("catalogo"));
+    nome.value = dados[indice].nome;
+    resumo.value = dados[indice].descricao;
+    fotoa = dados[indice].foto;
 
+}
+
+function editarenvio(evento){
+    evento.preventDefault();
+    if ((fotoa != foto.value)&&(foto.value != "")){
+        fenvio()
+        .then(result =>{
+                        if(result){
+                            salvarEdicao(nomeArq);
+                        }
+                    });
+                   }
+}
+
+var fotoa;
 var nomeArq;
+
 async function fenvio() {
     const url = 'http://localhost:3005/upload';
     const arquivo = document.getElementById("foto").files[0];
---
+++ catalogo.js
@@ -0,0 +1,48 @@
+
+const cards = document.querySelector(".cards");
+
+carregarCatalogo();
+
+function carregarCatalogo(){
+    let dados = JSON.parse(localStorage.getItem("catalogo"));
+    let divcard = document.createElement("div");
+    if(dados == null){
+        divcard.innerHTML = "<p>Nenhum item cadastrado</p>";
+        cards.appendChild(divcard);
+        return null;
+    }
+
+dados.forEach((elemento, indice) => {
+    let divcard = document.createElement("div");
+    divcard.setAttribute("class", "card");
+    divcard.innerHTML = `<div class="info">
+    <img src="imagem/Trash can.png" onclick="excluir(${indice})" alt="">
+    <img src="imagem/Edit.png" onclick="editar(${indice})" alt="">
+    </div>
+
+    <div class="cardimg">
+    <img src="img/${elemento.foto}" alt="">
+    <p>${elemento.nome}</p>
+    <p>${elemento.descricao}</p>
+    </div>
+    `
+    cards.appendChild(divcard);
+});
+
+}
+function excluir(indice){
+    let dados = JSON.parse(localStorage.getItem("catalogo"));
+    if(dados.length == 1)
+    {localStorage.clear("catalogo");}
+    else{
+    dados.splice(indice,1);
+    localStorage.setItem("catalogo", JSON.stringify(dados));
+    }
+    window.location.reload();
+}
+
+function editar(indice){
+    var url = "cadastroitem.html?peditar=true&indice="+
+    encodeURIComponent(indice);
+    window.location.href = url;
+}
